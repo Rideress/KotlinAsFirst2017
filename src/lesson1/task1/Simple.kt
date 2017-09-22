e848bd88d62285f9977c2f0ae3af93706136cf8f
Роман <phillroman24@gmail.com>
2017-09-22 19:19:07 +0300

    Учел комментарии и исправил ошибку.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -73,7 +73,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
 fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
-        grad *(PI / 180) + min * (PI /( 180 * 60)) + sec * (PI / (180 * 60 * 60))
+        (grad + (min / 60.0) + (sec / 3600.0)) * PI / 180.0
 
 
 /**
@@ -82,9 +82,9 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =(
-        sqrt(sqr(x2 - x1) + sqr(y2-y1) )
-)
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+        sqrt(sqr(x2 - x1) + sqr(y2-y1))
+
 
 
 /**
