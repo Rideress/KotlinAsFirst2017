9e492c145e75678bb5a28810c83de83f3527e7a0
Роман <phillroman24@gmail.com>
2017-10-03 21:24:46 +0300

    Решил часть задач второго урока (бновление).

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -48,4 +48,14 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    when  {
+        a <= r && b <= s -> return true
+        a <= s && b <= r -> return true
+        a <= r && c <= s -> return true
+        a <= s && c <= r -> return true
+        c <= s && b <= r -> return true
+        c <= r && b <= s -> return true
+        else             -> return false
+    }
+}
